6d6fefbb4b19784ddc2606066540a71af39da8a5
Aaron Pham <29749331+aarnphm@users.noreply.github.com>
2022-06-09 22:23:09 -0400

    fix: operator config should not be imported internally
    Signed-off-by: Aaron Pham <29749331+aarnphm@users.noreply.github.com>

--- bentoctl_aws_ec2/create_deployable.py
+++ bentoctl_aws_ec2/create_deployable.py
@@ -1,45 +1,12 @@
-import os
+from __future__ import annotations
 
-from bentoctl.exceptions import BentoctlException
-
-BENTOML_DOCKER_FILE_PATH = "./env/docker/Dockerfile"
+from typing import Any
 
 
 def create_deployable(
     bento_path: str,
     destination_dir: str,
-    bento_metadata: dict,
-    overwrite_deployable=None,
+    bento_metadata: dict[str, Any],
+    overwrite_deployable: bool,
 ):
-    """
-    The deployable is the bento along with all the modifications (if any)
-    requried to deploy to the cloud service.
-
-    Parameters
-    ----------
-    bento_path: str
-        Path to the bento from the bento store.
-    destination_dir: str
-        directory to create the deployable into.
-    bento_metadata: dict
-        metadata about the bento.
-
-    Returns
-    -------
-    dockerfile_path : str
-        path to the dockerfile.
-    docker_context_path : str
-        path to the docker context.
-    additional_build_args : dict
-        Any addition build arguments that need to be passed to the
-        docker build command
-    """
-    docker_file_path = os.path.join(bento_path, BENTOML_DOCKER_FILE_PATH)
-    if not os.path.exists(docker_file_path):
-        raise BentoctlException(
-            "Could not locate Dockerfile in the bento_path provided."
-            f"dockerfile: {docker_file_path}, bento_path: {bento_path}"
-        )
-
-    additional_build_args = None
-    return docker_file_path, bento_path, additional_build_args
+    pass
--- operator_config.py
+++ operator_config.py
@@ -1,4 +1,46 @@
-from bentoctl_aws_ec2.schema import OPERATOR_SCHEMA
+OPERATOR_SCHEMA = {
+    "region": {
+        "required": True,
+        "type": "string",
+        "default": "us-west-1",
+        "help_message": "AWS region to which you want to deploy",
+    },
+    "instance_type": {
+        "type": "string",
+        "default": "t2.micro",
+        "help_message": "Instance type for the EC2 deployment."
+        " See https://aws.amazon.com/ec2/instance-types/ for the entire list.",
+    },
+    "ami_id": {
+        "type": "string",
+        "default": "ami-0a3277ffce9146b74",
+        "help_message": "Amazon Machine Image (AMI) used for the EC2 instance. "
+        "Check out https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/AMIs.html "
+        "for more information.",
+    },
+    "enable_gpus": {
+        "type": "boolean",
+        "default": False,
+        "coerce": bool,
+        "help_message": "If using GPU-accelerated instance_types then ennable this.",
+    },
+    "environment_variables": {
+        "type": "list",
+        "schema": {
+            "type": "dict",
+            "schema": {
+                "name": {
+                    "type": "string",
+                    "help_message": "Name for environment variable",
+                },
+                "value": {
+                    "type": "string",
+                    "help_message": "Value for the environment variables",
+                },
+            },
+        },
+    },
+}
 
 OPERATOR_NAME = "aws-ec2"
 OPERATOR_MODULE = "bentoctl_aws_ec2"
