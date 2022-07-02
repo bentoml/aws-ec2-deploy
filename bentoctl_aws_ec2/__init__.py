9bf54f77cefe68dcb3b8a8cb19f895d70c14022f
jjmachan <jamesjithin97@gmail.com>
2022-07-02 20:35:50 +0530

    create deployable

--- bentoctl_aws_ec2/__init__.py
+++ bentoctl_aws_ec2/__init__.py
@@ -1,5 +1,8 @@
-from bentoctl_aws_ec2.create_deployable import create_deployable
+from bentoctl.utils.operator_helpers import \
+    create_deployable_from_local_bentostore as create_deployable
+
 from bentoctl_aws_ec2.generate import generate
-from bentoctl_aws_ec2.registry_utils import create_repository, delete_repository
+from bentoctl_aws_ec2.registry_utils import (create_repository,
+                                             delete_repository)
 
 __all__ = ["generate", "create_deployable", "create_repository", "delete_repository"]
